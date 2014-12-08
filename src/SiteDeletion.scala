a4155390500a8f8c1f2a119fd64d5f6134ddd4b9
Sinan Halilbeyoglu <sinan.halilbeyoglu@gmail.com>
2014-12-08 22:38:37 +0000

    Creating site-deletion project

---
+++ src/SiteDeletion.scala
@@ -0,0 +1,12 @@
+/**
+ * Created by Sinan on 08/12/2014.
+ */
+class SiteDeletion {
+
+  /** *
+    * Drops all tables that contain the site's name in the table name
+    * @param siteName Name of site to be deleted
+    * @return
+    */
+  def dropSitenameTables(siteName: String)
+}
